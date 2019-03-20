d4be385bec9a94d1e312dea25f4f8d3fe4055028
trevjnels <trevorjohnnels@gmail.com>
2019-03-20 13:17:50 -0600

    need assistance

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 "use strict";
+
 const logger = function(func) {
   console.log(func.name, " is running!");
 };
+
+//serach parks in multiple states
+//user must control max number of results with default of 10
+//each park listing must have: full name,  descritpion, URL, real address
+//  and photo
+//search must wipeout the moment someone submits form or types in input
 
 const copyrightListner = function() {
   logger(copyrightListner);
